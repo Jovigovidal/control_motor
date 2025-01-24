a5abad8edd7d6e304c9daa27ccf0a8b162970ceb
JovigoVidal <jovi_go@outlook.com>
2025-01-24 00:14:09 -0500

    test motor 28BYJ-48

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,53 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int IN1 = 18;
+int IN2 = 19;
+int IN3 = 21;
+int IN4 = 22;
+
+void stepMotor(int step);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Configurar los pines como salida
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Secuencia para mover el motor paso a paso
+  for (int i = 0; i < 512; i++) {  // Hacer 512 pasos (ajustable según el motor)
+    stepMotor(i % 4);
+    delay(3); // Retardo entre pasos
+  }
 }
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// Función para mover el motor un paso
+void stepMotor(int step) {
+  switch (step) {
+    case 0:
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      break;
+    case 1:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      break;
+    case 2:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      break;
+    case 3:
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      break;
+  }
 }
